2b655163e167332b1a0daf5103d4db127f49688e
WilsonPsychedelic <michaelbrooks452@gmail.com>
2026-05-21 18:29:50 -0700

    Added django-cms-project

---
+++ myproject/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.i18n import JavaScriptCatalog
+
+urlpatterns = i18n_patterns(
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+    path('admin/', admin.site.urls),
+    path('filer/', include('filer.urls')),
+    path('', include('cms.urls')),
+)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
